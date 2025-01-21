7a4df5046198e76eb87b8d72bd4ef816ff1c9d75
GiToon10100011 <Boon10034@gmail.com>
2025-01-22 02:02:16 +0900

    250122 Add CORS Configs

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,10 @@ const PORT = 4000;
 const app = express();
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://jinu-sportfolioconsole.web.app",
-      "https://jinuportfolio-server.com",
-    ],
+    origin: "https://jinu-sportfolioconsole.web.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
